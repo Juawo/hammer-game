9166431f1d6324f0e55f576b058a03a233cefd28
Juawo <joaopedrojp2703@gmail.com>
2026-09-11 17:57:11 -0300

    Update player mechanics with speed and jump modifiers; adjust project time tracker values

--- scripts/Player/player.gd
+++ scripts/Player/player.gd
@@ -5,6 +5,9 @@ const JUMP_VELOCITY = -550.0
 const ACCELERATION = 1500.0
 const FRICTION = 2000.0
 
+var speed_modifier := 1.0
+var jump_modifier := 1.0
+
 const INVULNERABILITY_TIME = 2
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
@@ -18,6 +21,7 @@ var knockback_velocity := Vector2.ZERO
 func _ready() -> void:
 	current_health = max_health
 
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -25,11 +29,11 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = (JUMP_VELOCITY * jump_modifier)
 		
 	var direction := Input.get_axis("left", "right")
 	if direction:
-		velocity.x = move_toward(velocity.x, direction * MAX_SPEED, ACCELERATION * delta)
+		velocity.x = move_toward(velocity.x, direction * (MAX_SPEED * speed_modifier), ACCELERATION * delta)
 		sprite_2d.flip_h = direction < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
@@ -39,9 +43,20 @@ func _physics_process(delta: float) -> void:
 		knockback_velocity = knockback_velocity.move_toward(Vector2.ZERO, FRICTION * 1.5 * delta)
 		
 	move_and_slide()
+	
 
-func apply_knocknack(force : Vector2) -> void :
+func apply_knockback(force : Vector2) -> void :
 	knockback_velocity = force
+
+func set_hammer_charging(is_charging : bool) -> void :
+	if is_charging :
+		speed_modifier = 0.4
+		jump_modifier = 0.6
+	else :
+		speed_modifier = 1.0
+		jump_modifier = 1.0
+		
+		
 
 func take_damage(value : int) -> void :
 	if is_invulnerable :
