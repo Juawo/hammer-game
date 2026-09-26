0809e4f766c5df5156c5d15d26553f4a66a5adc9
Juawo <joaopedrojp2703@gmail.com>
2026-09-26 14:37:13 -0300

    Update level logic with enemy spawning and dynamic difficulty adjustments

--- scripts/Levels/level_base.gd
+++ scripts/Levels/level_base.gd
@@ -12,6 +12,7 @@ var is_escaping := false
 
 func _ready() -> void:
 	if breakable_wall:
+		breakable_wall.wall_cracked.connect(trigger_wall_cracked_sequence)
 		breakable_wall.wall_destroyed.connect(trigger_escape_sequence)
 		
 	if level_exit:
@@ -20,19 +21,35 @@ func _ready() -> void:
 func can_spawn_enemy() -> bool :
 	return current_enemies < max_enemies_allowed
 
-func on_enemy_died() -> void :
+func _on_enemy_died() -> void :
 	current_enemies -= 1
 
+func register_enemy(enemy: Node) -> void :
+	current_enemies += 1
+	enemy.tree_exited.connect(_on_enemy_died)
+
+# when the wall can be take damage | get the game more frenetic
+func trigger_wall_cracked_sequence() -> void:
+	max_enemies_allowed *= 2
+	update_spawners_generation(2.5)
+
+# when the wall is destroyed | get the game the most frenetic momment
 func trigger_escape_sequence() -> void :
 	is_escaping = true
 	max_enemies_allowed *= 2
 	level_exit.switch_disable_exit()
+	update_spawners_generation(1.5)
 	label.visible = true
 	var tween = create_tween().tween_property(label, "scale", Vector2(1.5,1.5), 1)
 	tween.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_ELASTIC)
-	
+
+func update_spawners_generation(new_interval: float) -> void:
+	var spawners = get_tree().get_nodes_in_group("Spawners")
+	for spawner in spawners :
+		if spawner.has_method("set_frenetic_mode") :
+			spawner.set_frenetic_mode(new_interval)
+	#  add juice and animations here, for get more frenetic emotion in the player
 
 func _on_level_finished() -> void :
 	print("NEXT LEVEL!")
 	# add level transition / add juice/animation
-	
